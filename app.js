e524a2e52e7e3dcb17d679ff868e3f7d3666b5fe
Fleksis <markuss0303@gmail.com>
2021-03-16 16:11:23 +0200

    11 video

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const list = document.querySelector('book-list ul');
 
 //delete books
 list.addEventListener('click', function(e){
-  if(e.target.classname == 'delete'){
+  if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    li.parentnode.removeChild(li);
+    li.removeChild(li);
   }
+});
+
+// add book-list
+const addForm = forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
 });
