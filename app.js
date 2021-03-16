0bb2027424a3a9b006ac094dfbc49a0d69c9f2fd
Fleksis <markuss0303@gmail.com>
2021-03-16 16:43:09 +0200

    12 video + safixotas dažas rindas

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const list = document.querySelector('book-list ul');
+const list = document.querySelector('#book-list ul');
 
 //delete books
 list.addEventListener('click', function(e){
   if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    li.removeChild(li);
+    list.removeChild(li);
   }
 });
 
 // add book-list
-const addForm = forms['add-book'];
-
+const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
   const value = addForm.querySelector('input[type="text"]').value;
-  console.log(value);
+
+  //create elemets
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+//add content
+deleteBtn.textContent = 'delete';
+bookName.textContent = value;
+
+  // append to document 
+li.appendChild(bookName);
+li.appendChild(deleteBtn)
+list.appendChild(li);
+
+
 });
